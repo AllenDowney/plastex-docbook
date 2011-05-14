5c8a70a8b7c326c09a8dc839e7ebec87cb76f75a
Tim Arnold <jtim.arnold@gmail.com>
2011-05-14 09:57:45 -0400

    minimal renderer

---
+++ __init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import re
+from plasTeX.Renderers.PageTemplate import Renderer as _Renderer
+
+class DocBook45(_Renderer):
+    """ Renderer for DocBook Version 4.5 documents """
+    fileExtension = '.xml'
+    imageTypes = ['.png','.jpg','.jpeg','.gif','.pdf']
+    vectorImageTypes = ['.svg']
+
+    def cleanup(self, document, files, postProcess=None):
+        res = _Renderer.cleanup(self, document, files, postProcess=postProcess)
+        return res
+
+    def processFileContent(self, document, s):
+        s = _Renderer.processFileContent(self, document, s)
+        return s
+    
+Renderer = DocBook45
